78af2fa2a233e64af3cc5e9956203645dffb39b2
Hilmi AM <imiia75775@proton.me>
2024-06-26 07:03:44 +0700

    configuration properties

--- src/main/java/franxx/code/spring/validation/SpringValidationApplication.java
+++ src/main/java/franxx/code/spring/validation/SpringValidationApplication.java
@@ -1,13 +1,18 @@
 package franxx.code.spring.validation;
 
+import franxx.code.spring.validation.properties.Database;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties({
+        Database.class
+})
 @SpringBootApplication
 public class SpringValidationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringValidationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringValidationApplication.class, args);
+    }
 
 }
